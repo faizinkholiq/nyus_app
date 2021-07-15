53fd1dbf78a5c754d77675b07c9c9ac4805f3841
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-07-16 02:36:11 +0700

    update tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,174 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:nyus_app/home_screen.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // runApp(MyApp());
-  runApp(TabBarExample());
-}
+void main() => runApp(const MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const list = [
-    Locale('en'),
-    Locale('ar'),
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
   ];
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Zoom Drawer Demo',
-      onGenerateTitle: (context) => "app_name",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // ignore: deprecated_member_use
-        accentColor: Colors.orange,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => MenuProvider(),
-        child: HomeScreen(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-    );
-  }
-}
-
-class MenuItem {
-  final String title;
-  final IconData icon;
-  final int index;
-
-  const MenuItem(this.title, this.icon, this.index);
-}
-
-class TabBarExample extends StatefulWidget {
-  @override
-  _TabBarExampleState createState() => _TabBarExampleState();
-}
-
-
-class _TabBarExampleState extends State<TabBarExample> with SingleTickerProviderStateMixin {
-  static List<MenuItem> mainMenu = [
-    MenuItem("Home", Icons.home, 0),
-    MenuItem("Search", Icons.search, 1),
-    MenuItem("Headline", Icons.map_outlined, 2),
-    MenuItem("Profile", Icons.person, 3),
-  ];
-
-  late TabController _controller;
-  late int _index;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = new TabController(length: mainMenu.length, vsync: this);
-    _index = 0;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xFFFFFFFF),
-            title: Text(
-              'Nyus App.',
-              style: TextStyle(
-                color: Color(0xFF1C2447),
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            bottom: TabBar(
-              // isScrollable: true,
-              tabs: mainMenu.map(
-                (item) => Tab(icon: Icon(item.icon)),
-              ).toList(),
-              labelColor: Color(0xFF1C2447),
-            ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          body: TabBarView(
-            controller: _controller,
-            children: [
-              NewPage(_index),
-              HotelsPage(_index),
-              FoodPage(_index),
-              FunPage(_index),
-            ]
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.white,
-            showSelectedLabels: false, 
-            showUnselectedLabels: false,
-            selectedIconTheme: IconThemeData(color: Colors.black,),
-            unselectedIconTheme: IconThemeData(color: Colors.grey[400]),
-            type: BottomNavigationBarType.fixed,
-            elevation: 0,
-            currentIndex: _index,
-            items: mainMenu.map(
-              (item) => BottomNavigationBarItem(
-                label: item.title,
-                icon: Icon(
-                  item.icon,
-                ),
-              ),
-            ).toList(),
-            onTap: (int _index) {
-              setState(() => this._index = _index);
-            },
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
           ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
-    );
-  }
-}
-
-class NewPage extends StatelessWidget {
-  final int index;
-
-  NewPage(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new Text('NewPage, index: $index'),
-    );
-  }
-}
-
-class HotelsPage extends StatelessWidget {
-  final int index;
-
-  HotelsPage(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new Text('HotelsPage, index: $index'),
-    );
-  }
-}
-
-class FoodPage extends StatelessWidget {
-  final int index;
-
-  FoodPage(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new Text('FoodPage, index: $index'),
-    );
-  }
-}
-
-class FunPage extends StatelessWidget {
-  final int index;
-
-  FunPage(this.index);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new Text('FunPage, index: $index'),
     );
   }
 }
